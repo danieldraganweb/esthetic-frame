887cedc42937f288c56ed2756673e390889e5723
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-08-21 19:48:41 +0200

    create and style hero section component

---
+++ app/components/Hero.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/components/Hero.module.scss";
+import Link from "next/link";
+
+function HeroSection() {
+  return (
+    <>
+      <div className={styles["hero-image-container"]}>
+        <Image
+          src="/Banner.jpg"
+          alt="hero"
+          width={1100}
+          height={700}
+          sizes="100vw"
+          priority={true}
+          style={{
+            objectFit: "cover",
+            borderRadius: "1em",
+          }}
+          className={styles["hero-image"]}
+        />
+      </div>
+      <div className={styles["hero-container"]}>
+        <h1 className={styles["hero-title"]}>
+          Enhance Your Beauty with Our Permanent Makeup
+        </h1>
+        <p className={styles["hero-subtitle"]}>
+          Experience the convenience and confidence of flawless makeup that
+          lasts all day, every day.
+        </p>
+        <div className={styles["btn-container"]}>
+          <Link href="/services">
+            <button className={styles["hero-btn-1"]}>Learn More</button>
+          </Link>
+          <Link href="/book">
+            <button className={styles["hero-btn-2"]}>Book Now</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.scss";
-import Navbar from "./components/Navbar";
+import HeroSection from "./components/Hero";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Navbar />
+      <HeroSection />
       <h1 className={styles.title}>Welcome to Esthetic Frame</h1>
     </main>
   );
