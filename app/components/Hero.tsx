ccf6f7bb80c685811c2519a570bd4fb46d98d9a7
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-08-22 17:48:22 +0200

    rplace hero image, style hero banner

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,36 +6,38 @@ import Link from "next/link";
 function HeroSection() {
   return (
     <>
-      <div className={styles["hero-image-container"]}>
-        <Image
-          src="/Banner.jpg"
-          alt="hero"
-          width={1200}
-          height={700}
-          sizes="100vw"
-          priority={true}
-          style={{
-            objectFit: "cover",
-            borderRadius: "1em",
-          }}
-          className={styles["hero-image"]}
-        />
-      </div>
       <div className={styles["hero-container"]}>
-        <h1 className={styles["hero-title"]}>
-          Enhance Your Beauty with Our Permanent Makeup
-        </h1>
-        <p className={styles["hero-subtitle"]}>
-          Experience the convenience and confidence of flawless makeup that
-          lasts all day, every day.
-        </p>
-        <div className={styles["btn-container"]}>
-          <Link href="/services">
-            <button className={styles["hero-btn-1"]}>Learn More</button>
-          </Link>
-          <Link href="/book">
-            <button className={styles["hero-btn-2"]}>Book Now</button>
-          </Link>
+        <div className={styles["hero-image-container"]}>
+          <Image
+            src="/Hero.jpg"
+            alt="hero"
+            width={1200}
+            height={700}
+            sizes="100vw"
+            priority={true}
+            style={{
+              objectFit: "cover",
+              borderRadius: "1em",
+            }}
+            className={styles["hero-image"]}
+          />
+        </div>
+        <div className={styles["hero-text-container"]}>
+          <div className={styles["hero-text-overlay"]}>
+            <h1 className={styles["hero-title"]}>
+              Enhance Your Beauty with Our Permanent Makeup
+            </h1>
+            <p className={styles["hero-subtitle"]}>
+              Experience the convenience and confidence of flawless makeup that
+              lasts all day, every day.
+            </p>
+            <Link href="/services">
+              <button className={styles["hero-btn-1"]}>Learn More</button>
+            </Link>
+            <Link href="/book">
+              <button className={styles["hero-btn-2"]}>Book Now</button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
