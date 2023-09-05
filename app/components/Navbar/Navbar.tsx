132bbd2556043be7809f7bea307c04f8775aff22
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-09-05 17:07:22 +0200

    style navbar desktop links

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,31 +1,84 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import styles from "./Navbar.module.scss";
 import Link from "next/link";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <nav className={styles["navbar"]}>
         <div className={styles["hamburger-container"]}>
-          <button className={styles["hamburger"]}>
+          {/* <button className={styles["hamburger"]} onClick={toggleMenu}>
             <div className={styles["bar"]}></div>
+          </button> */}
+          <button
+            className={styles.burgerMenuButton}
+            onClick={toggleMenu}
+            aria-label="Toggle Menu"
+            aria-expanded={menuOpen ? "true" : "false"}
+          >
+            <svg
+              fill="var(--button-color)"
+              className={styles.hamburger}
+              viewBox="0 0 90 90"
+              width="22"
+            >
+              <rect
+                className={styles.lineTop}
+                width="88"
+                height="10"
+                x="1"
+                y="10"
+                rx="5"
+              />
+              <rect
+                className={styles.lineMiddle}
+                width="88"
+                height="10"
+                x="1"
+                y="40"
+                rx="5"
+              />
+              <rect
+                className={styles.lineBottom}
+                width="88"
+                height="10"
+                x="1"
+                y="70"
+                rx="5"
+              />
+            </svg>
           </button>
         </div>
         <div className={styles["navbar-links-left"]}>
-          <ul>
+          <ul
+            className={`${styles.navLinks} ${
+              menuOpen ? styles.open : styles.closed
+            }`}
+          >
             <li>
-              <Link href="/menu">
+              <Link href="/menu" key="menu" onClick={() => setMenuOpen(false)}>
                 <h1>Menu</h1>
               </Link>
             </li>
             <li>
-              <Link href="/gallery">
+              <Link
+                href="/gallery"
+                key="gallery"
+                onClick={() => setMenuOpen(false)}
+              >
                 <h1>Gallery</h1>
               </Link>
             </li>
             <li>
-              <Link href="/blog">
+              <Link href="/blog" key="blog" onClick={() => setMenuOpen(false)}>
                 <h1>Blog</h1>
               </Link>
             </li>
@@ -35,21 +88,33 @@ function Navbar() {
           <Logo />
         </div>
         <div className={styles["navbar-links-right"]}>
-          <ul>
+          <ul
+            className={`${styles.navLinks} ${
+              menuOpen ? styles.open : styles.closed
+            }`}
+          >
             <li>
-              <Link href="/about">
+              <Link
+                href="/about"
+                key="about"
+                onClick={() => setMenuOpen(false)}
+              >
                 <h1>About</h1>
               </Link>
             </li>
             <li>
-              <Link href="/contact">
+              <Link
+                href="/contact"
+                key="contact"
+                onClick={() => setMenuOpen(false)}
+              >
                 <h1>Contact</h1>
               </Link>
             </li>
-            <Link href="/book">
-              <button className={styles["navbar-button"]}>Book</button>
-            </Link>
           </ul>
+          <Link href="/book">
+            <button className={styles["navbar-button"]}>Book</button>
+          </Link>
         </div>
       </nav>
     </>
