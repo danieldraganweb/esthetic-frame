93b74781f5b80e4ad5f06f159153d2b0772e8af8
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-09-05 12:24:36 +0200

    add & style hamburger menu

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -7,30 +7,49 @@ function Navbar() {
   return (
     <>
       <nav className={styles["navbar"]}>
+        <div className={styles["hamburger-container"]}>
+          <button className={styles["hamburger"]}>
+            <div className={styles["bar"]}></div>
+          </button>
+        </div>
         <div className={styles["navbar-links-left"]}>
-          <Link href="/menu">
-            <h1>Menu</h1>
-          </Link>
-          <Link href="/gallery">
-            <h1>Gallery</h1>
-          </Link>
-          <Link href="/blog">
-            <h1>Blog</h1>
-          </Link>
+          <ul>
+            <li>
+              <Link href="/menu">
+                <h1>Menu</h1>
+              </Link>
+            </li>
+            <li>
+              <Link href="/gallery">
+                <h1>Gallery</h1>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <h1>Blog</h1>
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className={styles["navbar-logo"]}>
           <Logo />
         </div>
         <div className={styles["navbar-links-right"]}>
-          <Link href="/about">
-            <h1>About</h1>
-          </Link>
-          <Link href="/contact">
-            <h1>Contact</h1>
-          </Link>
-          <Link href="/book">
-            <button className={styles["navbar-button"]}>Book</button>
-          </Link>
+          <ul>
+            <li>
+              <Link href="/about">
+                <h1>About</h1>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <h1>Contact</h1>
+              </Link>
+            </li>
+            <Link href="/book">
+              <button className={styles["navbar-button"]}>Book</button>
+            </Link>
+          </ul>
         </div>
       </nav>
     </>
