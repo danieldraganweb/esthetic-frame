4a7f8e08c0bf5e8f8e6b26dd5a6f6b7cf920a38f
Daniel Dragan <110283149+danieldraganweb@users.noreply.github.com>
2023-10-03 14:57:11 +0200

    update next, style logo mobile+desktop (#14)
    update next and style logo

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import Image from "next/image";
 import styles from "./Navbar.module.scss";
 import Link from "next/link";
 
@@ -7,7 +6,6 @@ function LogoComponent() {
   return (
     <>
       <div className={styles.logo}>
-        <div className={styles.logo__square}></div>
         <Link href="/">
           <h1>ESTHETIC FRAME</h1>
           <h2>by Alexandra Ivan</h2>
