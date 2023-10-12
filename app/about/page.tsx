a2d434be510d951788323c69376b51e6a7014055
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-10-12 15:42:59 +0200

    edit layout and content for about and contact page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,108 +8,30 @@ function About() {
     <>
       <main>
         <div className={styles["about-container"]}>
-          <div className={styles["about-image-container"]}>
-            <Image
-              src="/about.jpg"
-              alt="About"
-              width={1920}
-              height={1080}
-              layout="responsive"
-              sizes="(max-width: 1100px) 100vw, 1920px"
-            ></Image>
-            <h1 className={styles.title}></h1>
-            <h1></h1>
-          </div>
-          <ul className={styles.highlights}>
-            <li className={styles["hero"]}>
-              <h1>Discover Your Natural Radiance</h1>
-              <p> Welcome to Esthetic Frame!</p>
-              <p>Your Permanent Makeup Studio in Vienna</p>
-              <p>
-                At Esthetic Frame, we are more than just a Permanent Makeup
-                studio; we are your dedicated partners in enhancing your natural
-                beauty. Located in the heart of Vienna, our studio is where
-                lasting elegance and confidence meet. We invite you to embark on
-                a journey with us, where every visit leaves you feeling
-                empowered and beautiful.
-              </p>
-            </li>
-            <li>
-              <h1>Expertise in Permanent Makeup</h1>
-              <p>
-                Our team of skilled artists possesses a deep understanding of
-                Permanent Makeup techniques, ensuring that every stroke and
-                detail enhances your features naturally.
-              </p>
-            </li>
-            <li>
-              <h1>Safe and Quality Products</h1>
-              <p>
-                We prioritize your well-being, which is why we use only the
-                highest quality, safe, and non-allergenic pigments and
-                materials.
-              </p>
-            </li>
-            <li>
-              <h1>Welcoming Atmosphere</h1>
-              <p>
-                Step into our studio, and you&rsquo;ll immediately feel at ease.
-                Our warm and inviting ambiance is designed to provide you with a
-                comfortable and relaxing experience.
-              </p>
-            </li>
-            <li>
-              <h1>Personalized Service</h1>
-              <p>
-                Your unique beauty deserves a personalized approach. We listen
-                to your preferences and work closely with you to achieve the
-                results you desire.
-              </p>
-            </li>
-            <li>
-              <h1>Passion for Perfection</h1>
-              <p>
-                We are passionate about what we do, and we strive for perfection
-                in every detail. We are committed to ensuring that you leave our
-                studio feeling confident and beautiful.
-              </p>
-            </li>
-            <li>
-              <h1>Discover Your Natural Radiance</h1>
-              <p className={styles.description}>
-                Discover Esthetic Frame, where artistry meets beauty and every
-                visit is an opportunity to uncover your natural radiance. Our
-                journey is your journey, and we&rsquo;re excited to share it
-                with you.
-              </p>
-            </li>
-            <li>
-              <h1>Meet Our Team</h1>
-              <p className={styles.description}>
-                Ready to discover the Esthetic Frame experience? Scroll down to
-                meet our team and learn about our journey.
-              </p>
-            </li>
-          </ul>
           <div className={styles["team-container"]}>
             <div className={styles["team-member-container"]}>
               <div className={styles["team-image-container"]}>
                 <Image
                   src="/team-member-1.jpg"
                   alt="Team Member 1"
-                  width={500}
+                  width={335}
                   height={500}
-                  layout="responsive"
+                  // layout="responsive"
                   sizes="(max-width: 500px) 100vw, 500px"
+                  loading="eager"
+                  // style={{
+                  //   borderRadius: "50%",
+                  //   objectFit: "cover",
+                  // }}
                 ></Image>
                 <div className={styles["team-member-hero"]}>
                   <h1 className={styles.description}>
                     I&rsquo;m Alexandra Ivan, founder and permanent makeup
                     artist at Esthetic Frame. I&rsquo;m so excited to share my
-                    journey with you, and I can&rsquo;t wait to help you
-                    discover your natural radiance. Welcome to the world of
-                    timeless beauty, where passion meets precision and the art
-                    of transformation knows no bounds.
+                    journey with you and I can&rsquo;t wait to help you discover
+                    your natural radiance. Welcome to the world of timeless
+                    beauty, where passion meets precision and the art of
+                    transformation knows no bounds.
                   </h1>
                 </div>
               </div>
