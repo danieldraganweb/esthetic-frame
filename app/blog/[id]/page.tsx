8e1b9c6bfefd3b797536a8a98752d3adb4ed6a2d
Daniel Dragan <110283149+danieldraganweb@users.noreply.github.com>
2023-10-15 20:47:52 +0200

    style blog and slug pages (#44)

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,52 +1,86 @@
 "use client";
 import React from "react";
-import styles from "../blog.module.scss";
+import styles from "../[id]/id.module.scss";
 import { useBlogPosts } from "../../hooks/useAllBlogPosts";
-import { BlogPostContent, BlogPost } from "../../types";
+// import { BlogPostContent, BlogPost } from "../../types";
 import Image from "next/image";
 import { useParams } from "next/navigation";
+import Loading from "@/app/loading";
+import Link from "next/link";
+import Contact from "@/app/components/Contact/Contact";
 
 const Post = () => {
   const { blogPosts, loading } = useBlogPosts();
   const params = useParams();
-
   const post = blogPosts.find((p) => p.id === params.id);
+
+  const titles = blogPosts.map((post) => post.fields?.name);
   return (
-    <div className={styles["blog-page"]}>
-      {loading && (
-        <div className={styles["loading"]}>
-          <p>Loading...</p>
-        </div>
-      )}
-      <h1 className={styles["blog-title"]}>Blog Article</h1>
-      {/* <p> {blogPosts.length} posts</p> */}
-      <div className={styles["blog-container"]}>
-        <div key={post?.id} className={styles.article}>
-          <Image
-            loading="lazy"
-            src={post?.fields?.image[0].url ?? ""}
-            alt={post?.fields?.name ?? ""}
-            width={post?.fields?.image[0].width}
-            height={post?.fields?.image[0].height}
-            layout="responsive"
-            style={{
-              borderRadius: "0.5em 0.5em 0 0",
-              boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
-            }}
-            sizes="(max-width: 1100px) 100vw, 600px"
-          />
+    <>
+      <main className={styles["blog-main"]}>
+        <div className={styles["blog-page"]}>
+          {loading && (
+            <div className={styles["loading"]}>
+              <Loading />
+            </div>
+          )}
 
-          <h2 className={styles["article-title"]}>{post?.fields?.name}</h2>
+          {/* <p> {blogPosts.length} posts</p> */}
+          <div className={styles["blog-container"]}>
+            <section>
+              <div className={styles["blog-hero"]}>
+                <Image
+                  loading="eager"
+                  src={post?.fields?.image[0].url ?? ""}
+                  alt={post?.fields?.name ?? ""}
+                  width={post?.fields?.image[0].width}
+                  height={post?.fields?.image[0].height}
+                  // layout="responsive"
+                  style={{
+                    boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+                  }}
+                  sizes="(max-width: 1100px) 100vw, 600px"
+                />
+                <h2 className={styles["article-title"]}>
+                  {post?.fields?.name}
+                </h2>
+              </div>
+            </section>
 
-          <p className={styles["article-date"]}>
-            Published on {post?.fields?.published}
-          </p>
-          <p className={styles["article-content"]}>
-            {post?.fields?.["blog-content"]}
-          </p>
+            <div key={post?.id} className={styles.article}>
+              <p className={styles["article-date"]}>
+                Published on {post?.fields?.published}
+              </p>
+              <p className={styles["article-content"]}>
+                {post?.fields?.["blog-content"]}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+        <div className={styles["blog-sidebar"]}>
+          <h2>All Blog Posts</h2>
+          <ul>
+            {titles.map((title, index) => (
+              <Link href={`/blog/${blogPosts[index].id}`} passHref key={index}>
+                <li className={styles["blog-sidebar-item"]}>
+                  <h1 className={styles["blog-sidebar-title"]}>
+                    #{index + 1}. {title}
+                  </h1>
+                  <p className={styles["blog-sidebar-date"]}>
+                    Published on {blogPosts[index].fields?.published}
+                  </p>
+                </li>
+              </Link>
+            ))}
+            {/* <div
+              className={`${styles["blog-sidebar-contact"]} ${styles["my-custom-class"]}`}
+            >
+              <Contact />
+            </div> */}
+          </ul>
+        </div>
+      </main>
+    </>
   );
 };
 export default Post;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -14,32 +14,28 @@ const Blog = () => {
   return (
     <div className={styles["blog-page"]}>
       <h1 className={styles["blog-title"]}>Blog</h1>
-      {/* <p> {blogPosts.length} posts</p> */}
       <div className={styles["blog-container"]}>
         {blogPosts.map((post: BlogPostContent) => (
           <div key={post.id} className={styles.article}>
-            <Image
-              loading="lazy"
-              src={post.fields?.image[0].url}
-              alt={post.fields?.name}
-              width={post.fields?.image[0].width}
-              height={post.fields?.image[0].height}
-              layout="responsive"
-              style={{
-                borderRadius: "0.5em 0.5em 0 0",
-                boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
-              }}
-              sizes="(max-width: 1100px) 100vw, 600px"
-            />
-            <Link href={`/blog/${post?.id}` || ""}>
+            <Link href={`/blog/${post.id}`} passHref>
+              <Image
+                loading="lazy"
+                src={post.fields?.image[0].url}
+                alt={post.fields?.name}
+                width={post.fields?.image[0].width}
+                height={post.fields?.image[0].height}
+                // layout="responsive"
+                style={{
+                  borderRadius: "0.5em 0.5em 0 0",
+                  boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+                }}
+                sizes="(max-width: 1100px) 100vw, 600px"
+              />
               <h2 className={styles["article-title"]}>{post.fields?.name}</h2>
+              <p className={styles["article-date"]}>
+                Published on {post.fields?.published}
+              </p>
             </Link>
-            <p className={styles["article-date"]}>
-              Published on {post.fields?.published}
-            </p>
-            {/* <p className={styles["article-content"]}>
-              {post.fields?.["blog-content"]}
-            </p> */}
           </div>
         ))}
       </div>
