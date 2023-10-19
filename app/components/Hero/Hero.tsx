ca1f6392eff30315eea3ec269e58abceeba4eadf
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-10-19 11:10:20 +0200

    Hero responsive design for tablet and desktop

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -6,40 +6,46 @@ import Link from "next/link";
 function HeroSection() {
   return (
     <>
-      <div className={styles["hero-container"]}>
-        <div className={styles["hero-image-container"]}>
-          <Image
-            src="/Hero.jpg"
-            alt="hero"
-            width={1200}
-            height={700}
-            sizes="100vw"
-            priority={true}
-            style={{
-              objectFit: "contain",
-              borderRadius: "1em",
-            }}
-            className={styles["hero-image"]}
-          />
-        </div>
-        <div className={styles["hero-text-container"]}>
-          <div className={styles["hero-text-overlay"]}>
-            <h1 className={styles["hero-title"]}>
-              Enhance Your Beauty with Our Permanent Makeup
-            </h1>
-            <p className={styles["hero-subtitle"]}>
-              Experience the convenience and confidence of flawless makeup that
-              lasts all day, every day.
-            </p>
-            <Link href="/services">
-              <button className={styles["hero-btn-1"]}>Learn More</button>
-            </Link>
-            <Link href="/book">
-              <button className={styles["hero-btn-2"]}>Book Now</button>
-            </Link>
+      <main className={styles["hero-section"]}>
+        <div className={styles["hero-container"]}>
+          <div className={styles["hero-image-container"]}>
+            <Image
+              src="/Hero.jpg"
+              alt="Permanent Make-up lady photo"
+              width={368}
+              height={533}
+              priority={true}
+              layout="responsive"
+              sizes="(max-width: 1100px) 100vw, 600px"
+              style={{
+                objectFit: "contain",
+              }}
+              className={styles["hero-image"]}
+            />
+          </div>
+          <div className={styles["hero-text-container"]}>
+            <div className={styles["hero-text-overlay"]}>
+              <h1 className={styles["hero-title"]}>
+                Steigere deine Schönheit mit unserem Permanent Make-up
+              </h1>
+              <p className={styles["hero-subtitle"]}>
+                Erlebe die Bequemlichkeit und das Selbstbewusstsein eines
+                makellosen Make-ups, das den ganzen Tag, jeden Tag hält!
+              </p>
+              <div className={styles["hero-button-container"]}>
+                <Link href="/book">
+                  <button className={styles["hero-btn-1"]}>Jetzt Buchen</button>
+                </Link>
+                <Link href="/services">
+                  <button className={styles["hero-btn-2"]}>
+                    Weitere Informationen
+                  </button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
