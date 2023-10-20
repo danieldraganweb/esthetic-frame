73aa998176a55ca5ab838c41a8f71c6ee2bf5fc0
Daniel Dragan <110283149+danieldraganweb@users.noreply.github.com>
2023-10-20 15:03:12 +0200

    style contact form component (#54)

--- app/components/ContactFormClientContact/ContactFormClientContact.tsx
+++ app/components/ContactFormClientContact/ContactFormClientContact.tsx
@@ -7,35 +7,21 @@ function ContactFormClientContact() {
   return (
     <>
       <main className={styles["contact-form-main"]}>
-        <div className={styles["contact-form-image-container"]}>
-          <Image
-            src="/anime-contact.jpg"
-            alt="contact-form"
-            width={400}
-            height={570}
-            sizes="100vw"
-            priority={true}
-            style={{
-              objectFit: "contain",
-              borderRadius: "1em",
-            }}
-            className={styles["contact-form-image"]}
-          />
-        </div>
         <div className={styles["contact-form-container"]}>
           <h1 className={styles["contact-form-title"]}>
-            Not sure what you need?
+            Nicht sicher, wonach Sie suchen?
           </h1>
           <p className={styles["contact-form-subtitle"]}>
-            Let us help you find the perfect solution for your beauty needs.
+            Lassen Sie uns Ihnen die perfekte Lösung für Ihre
+            Schönheitsbedürfnisse finden.
           </p>
-          <h1 className={styles["contact-form-subtitle"]}>
-            Leave your contact details and we will get back to you as soon as
-            possible.
-          </h1>
+          <p className={styles["contact-form-subtitle"]}>
+            Hinterlassen Sie Ihre Kontaktdaten, und wir melden uns so bald wie
+            möglich bei Ihnen.
+          </p>
           <input
             type="text"
-            placeholder="First Name (e.g. Maria)"
+            placeholder="Vorname (e.g. Maria)"
             required
             className={styles["contact-form-input"]}
             name="firstName"
@@ -46,7 +32,7 @@ function ContactFormClientContact() {
           ></label>
           <input
             type="text"
-            placeholder="Last Name (e.g. Musterfrau)"
+            placeholder="Nachname (e.g. Musterfrau)"
             required
             className={styles["contact-form-input"]}
             name="lastName"
@@ -68,7 +54,7 @@ function ContactFormClientContact() {
           ></label>
           <input
             type="tel"
-            placeholder="Phone (e.g. +43 xxx xxx xxxx.)"
+            placeholder="Telefon (e.g. +43 xxx xxx xxxx.)"
             required
             className={styles["contact-form-input"]}
             name="phone"
@@ -79,7 +65,7 @@ function ContactFormClientContact() {
           ></label>
           <Link href="/book">
             <button className={styles["contact-form-button"]}>
-              Book a Consultation
+              Einen Beratungstermin vereinbaren
             </button>
           </Link>
         </div>
