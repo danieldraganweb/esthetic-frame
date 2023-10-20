71863956a94251dd2f69dd5a4d6f3579b9a12285
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-10-20 17:58:10 +0200

    edit font size and opacity of contact form

--- app/components/ContactFormClientContact/ContactFormClientContact.tsx
+++ app/components/ContactFormClientContact/ContactFormClientContact.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./ContactFormClientContact.module.scss";
-import Image from "next/image";
 
 function ContactFormClientContact() {
   return (
