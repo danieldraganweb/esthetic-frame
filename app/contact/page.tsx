bac7685a45fe86b4114633b22c8e339c8ab03a87
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-10-23 21:34:33 +0200

    add new iframe for newsletter

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,6 +4,7 @@ import styles from "./contact.module.scss";
 import ContactFormClientContact from "../components/ContactFormClientContact/ContactFormClientContact";
 import Contact from "../components/Contact/Contact";
 import Image from "next/image";
+import NewsletterForm from "../components/Newsletter Form/NewsletterForm";
 
 function ContactPage() {
   return (
@@ -97,6 +98,24 @@ function ContactPage() {
         <section className={styles["contact-form-section"]}>
           <Contact />
           <ContactFormClientContact />
+          <div className={styles["newsletter-form"]}>
+            <div className={styles["newsletter-form-text"]}>
+              <h1>Newsletter</h1>
+              <p>
+                Bleiben Sie auf dem Laufenden mit den neuesten Nachrichten und
+                Angeboten von Esthetic Frame.
+              </p>
+              <p>
+                Bleiben Sie informiert und verpassen Sie keine Neuigkeiten!
+                Melden Sie sich für unseren Newsletter an, um die neuesten
+                Schönheitstipps, exklusive Angebote und mehr direkt in Ihrem
+                Posteingang zu erhalten.
+              </p>
+            </div>
+            <div className={styles["newsletter-form-iframe"]}>
+              <NewsletterForm />
+            </div>
+          </div>
         </section>
       </main>
     </>
