b918626f31521c125f84c5b76eb2ec6f91e123a5
Daniel Dragan <110283149+danieldraganweb@users.noreply.github.com>
2023-11-02 19:22:16 +0100

    sidebar gallery mobile test (#80)

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -7,10 +7,18 @@ import Image from "next/image";
 import disableScroll from "disable-scroll";
 import OpenMenuSVG from "../components/OpenMenuSVG";
 import CloseMenuSVG from "../components/CloseMenuSVG";
+import { useMediaQuery } from "react-responsive";
+import LoadingImage from "../components/Loading/LoadingImage";
 
 type Props = {};
 
 function Gallery(props: Props) {
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 });
+  const isBigScreen = useMediaQuery({ minWidth: 1824 });
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 768 });
+  const isPortrait = useMediaQuery({ orientation: "portrait" });
+  const isRetina = useMediaQuery({ minResolution: "2dppx" });
+
   const [category, setCategory] = useState<string | undefined>();
   const { images } = useGalleryImages();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -20,31 +28,30 @@ function Gallery(props: Props) {
 
   // Disable scrolling when sidebar is open
   useEffect(() => {
-    if (isSidebarOpen) {
+    if (isSidebarOpen && !isDesktopOrLaptop) {
       disableScroll.on();
     } else {
       disableScroll.off();
     }
-  }, [isSidebarOpen]);
+  }, [isSidebarOpen, isDesktopOrLaptop]);
+  const [loadedImages, setLoadedImages] = useState<Record<string, boolean>>({});
 
   return (
     <>
       <main className={styles["wrapper"]}>
         <div className={styles["gallery-container"]}>
-          <div className={styles["gallery-title-container"]}>
-            <h1 className={styles["gallery-title"]}>Galerie</h1>
-            {/* <p>
-            Blättern Sie durch unsere atemberaubenden
-            Vorher-Nachher-Verwandlungen
-          </p> */}
-            <button
-              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-              className={styles["sidebar-button"]}
-            >
-              {isSidebarOpen ? <CloseMenuSVG /> : <OpenMenuSVG />}
-            </button>
-          </div>
           <div className={styles["sidebar-container"]}>
+            <div className={styles["gallery-title-container"]}>
+              <h1 className={styles["gallery-title"]}>Galerie</h1>
+
+              <button
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                className={styles["sidebar-button"]}
+              >
+                {isSidebarOpen ? <CloseMenuSVG /> : <OpenMenuSVG />}
+              </button>
+            </div>
+
             <div
               className={`${styles["sidebar"]} ${
                 isSidebarOpen
@@ -53,15 +60,6 @@ function Gallery(props: Props) {
               }`}
             >
               <div className={styles["category-buttons"]} key="all">
-                <button
-                  key="all"
-                  onClick={() => {
-                    setCategory(undefined);
-                    setIsSidebarOpen(false);
-                  }}
-                >
-                  <h1>All</h1>
-                </button>
                 {categories.map((category) => (
                   <button
                     key={category}
@@ -70,9 +68,18 @@ function Gallery(props: Props) {
                       setIsSidebarOpen(false);
                     }}
                   >
-                    <h1>{category}</h1>
+                    {category}
                   </button>
                 ))}
+                <button
+                  key="all"
+                  onClick={() => {
+                    setCategory(undefined);
+                    setIsSidebarOpen(false);
+                  }}
+                >
+                  Alle
+                </button>
               </div>
             </div>
           </div>
@@ -84,17 +91,26 @@ function Gallery(props: Props) {
               )
               .map((image: GalleryImage) => (
                 <div key={image.id} className={styles.image}>
+                  {!loadedImages[image.id] && (
+                    <div className={styles.loader}>
+                      <LoadingImage />
+                    </div>
+                  )}
                   <Image
+                    onLoad={() =>
+                      setLoadedImages((prev) => ({ ...prev, [image.id]: true }))
+                    }
                     loading="lazy"
                     src={image.fields?.image[0].url}
                     alt={image.fields?.Name}
                     width={image.fields?.image[0].width}
                     height={image.fields?.image[0].height}
+                    className={styles["image"]}
                     style={{
                       borderRadius: "0.5em",
                       boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
                     }}
-                    sizes="(max-width: 1100px) 100vw, 600px"
+                    sizes="(max-width: 600px) 100vw, 600px"
                   />
                 </div>
               ))}
