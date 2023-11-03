63a28f08c468821b4981b4eef87976c3209682c5
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-11-03 16:35:28 +0100

    fix error import

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -8,7 +8,7 @@ import disableScroll from "disable-scroll";
 import OpenMenuSVG from "../components/OpenMenuSVG";
 import CloseMenuSVG from "../components/CloseMenuSVG";
 import { useMediaQuery } from "react-responsive";
-import Loading from "../components/Loading/Loading";
+// import Loading from "../components/Loading/Loading";
 import ImageModal from "../components/ImageModal/ImageModal";
 
 type Props = {};
@@ -111,11 +111,6 @@ function Gallery(props: Props) {
               )
               .map((image: GalleryImage) => (
                 <div key={image.id} className={styles.image}>
-                  {!loadedImages[image.id] && (
-                    <div className={styles.loader}>
-                      <Loading />
-                    </div>
-                  )}
                   <Image
                     onClick={() => setSelectedImage(image)}
                     onLoad={() =>
