285ea1ab7b326c5d04513b2df54533af6d469807
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-11-09 09:26:44 +0100

    Facebook Pixel implemented

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Services from "./components/Services/Services";
 import ContactFormClientContact from "./components/ContactFormClientContact/ContactFormClientContact";
 import BlogComponent from "./components/Blog/Blog";
 import CookiePolicyAlert from "./components/CookiePolicies/CookiePolicy";
+import FacebookPixel from "./components/FacebookPixel/FacebookPixel";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <ContactFormClientContact className={styles.contactForm} />
         <BlogComponent className={styles.blog} />
         <CookiePolicyAlert />
+        <FacebookPixel />
       </main>
     </>
   );
