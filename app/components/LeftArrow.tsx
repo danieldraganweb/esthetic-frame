5990bb2d4745a6c5aecd56506fce3aa6f3828c4c
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-11-20 19:52:31 +0100

    fix blog component homepage

--- app/components/LeftArrow.tsx
+++ app/components/LeftArrow.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const LeftArrow = () => {
   return (
     <svg
-      width="44px"
-      height="44px"
+      width="34px"
+      height="34px"
       viewBox="0 0 24.00 24.00"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
