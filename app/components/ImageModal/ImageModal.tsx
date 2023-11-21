b6f622e205429f207379d9f7641809e8d3ee11e2
Daniel Dragan <110283149+danieldraganweb@users.noreply.github.com>
2023-11-21 16:23:41 +0100

    add Image modal, style + responsivenes (#123)

--- app/components/ImageModal/ImageModal.tsx
+++ app/components/ImageModal/ImageModal.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GalleryImage } from "../../types";
 import styles from "./ImageModal.module.scss";
 import Image from "next/image";
+import RightArrow from "../RightArrow";
+import LeftArrow from "../LeftArrow";
+import CloseMenuSVG from "../CloseMenuSVG";
 
 type Props = {
   image: GalleryImage;
@@ -16,40 +19,61 @@ const ImageModal: React.FC<Props> = ({
   images,
   selectedIndex,
 }) => {
-  const [currentImage, setCurrentImage] = React.useState(image);
+  const [currentImage, setCurrentImage] = useState(image);
+  const [currentIndex, setCurrentIndex] = useState(selectedIndex);
+
+  useEffect(() => {
+    setCurrentImage(images[currentIndex]);
+  }, [currentIndex, images]);
 
   const handlePrev = () => {
-    if (selectedIndex > 0) {
-      setCurrentImage(images[selectedIndex - 1]);
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
     }
   };
 
   const handleNext = () => {
-    if (selectedIndex < images.length - 1) {
-      setCurrentImage(images[selectedIndex + 1]);
+    if (currentIndex < images.length - 1) {
+      setCurrentIndex(currentIndex + 1);
     }
   };
 
   const isModalOpen = !!currentImage;
 
   return (
-    <div
-      className={`${styles["sidebar"]} ${
-        isModalOpen ? styles["sidebar-open"] : styles["sidebar-closed"]
-      }`}
-    >
-      <div className={styles["image-modal"]} onClick={onClose}>
-        {/* <button onClick={handlePrev}>Previous</button> */}
-        <Image
-          loading="lazy"
-          src={currentImage.fields?.image[0].url}
-          alt={currentImage.fields?.Name}
-          width={currentImage.fields?.image[0].width}
-          height={currentImage.fields?.image[0].height}
-        />
-        {/* <button onClick={handleNext}>Next</button> */}
+    <>
+      <div
+        className={`${styles["sidebar"]} ${
+          isModalOpen ? styles["sidebar-open"] : styles["sidebar-closed"]
+        }`}
+      >
+        <div className={styles["backdrop"]} onClick={onClose}>
+          <div
+            className={styles["image-modal"]}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button onClick={handlePrev}>
+              <LeftArrow />
+            </button>
+            <Image
+              loading="lazy"
+              src={currentImage.fields?.image[0].url}
+              alt={currentImage.fields?.Name}
+              width={currentImage.fields?.image[0].width}
+              height={currentImage.fields?.image[0].height}
+              className={styles["modal-image"]}
+            />
+            <button onClick={handleNext}>
+              <RightArrow />
+            </button>
+          </div>
+        </div>
+        {/* <button onClick={onClose}>
+          <CloseMenuSVG />
+        </button>{" "}
+        Close button */}
       </div>
-    </div>
+    </>
   );
 };
 
